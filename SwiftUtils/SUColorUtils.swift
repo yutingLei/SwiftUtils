cda037a6800fee8988459a3bf28c23f02627aa71
yutingLei <lyt_930617@163.com>
2018-10-25 17:35:31 +0800

    init project & add files

---
+++ SwiftUtils/SUColorUtils.swift
@@ -0,0 +1,9 @@
+//
+//  SUColorUtils.swift
+//  SwiftUtils
+//
+//  Created by admin on 2018/10/25.
+//  Copyright © 2018 Develop. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftUtils/SUStringUtils.swift
@@ -0,0 +1,9 @@
+//
+//  SUStringUtils.swift
+//  SwiftUtils
+//
+//  Created by admin on 2018/10/25.
+//  Copyright © 2018 Develop. All rights reserved.
+//
+
+import Foundation
