83aa578fd5429e0ac06c79d31112f0922304ed6b
yutingLei <lyt_930617@163.com>
2018-10-25 17:44:49 +0800

    add Color&String utils

--- SwiftUtils/SUColorUtils.swift
+++ SwiftUtils/SUColorUtils.swift
@@ -7,3 +7,305 @@
 //
 
 import Foundation
+
+public extension UIColor {
+    /// AliceBlue
+    public class var aliceBlue: UIColor { get { return UIColor.rgb(240, 248, 255) } }
+    /// AntiqueWhite
+    public class var antiqueWhite: UIColor { get { return UIColor.rgb(250, 235, 215)} }
+    /// Aqua
+    public class var aqua: UIColor { get { return UIColor.rgb(0, 255, 255)} }
+    /// AquaMarine
+    public class var aquaMarine: UIColor { get { return UIColor.rgb(127, 255, 212)} }
+    /// Beige
+    public class var beige: UIColor { get { return UIColor.rgb(245, 245, 220)}}
+    /// Bisque
+    public class var bisque: UIColor { get { return UIColor.rgb(255, 228, 196)}}
+    /// BlanchedAlmond
+    public class var blanchedAlmond: UIColor { get { return UIColor.rgb(255, 235, 205)}}
+    /// BlueViolet
+    public class var blueViolet: UIColor { get { return UIColor.rgb(138, 43, 226)}}
+    /// Burlywood
+    public class var burlywood: UIColor { get { return UIColor.rgb(222, 184, 135)}}
+    /// CadetBlue
+    public class var cadetBlue: UIColor { get { return UIColor.rgb(95, 158, 160)}}
+    /// ChartReuse
+    public class var chartReuse: UIColor { get { return UIColor.rgb(127, 255, 0)}}
+    /// Chocolate
+    public class var chocolate: UIColor { get { return UIColor.rgb(201, 105, 30)}}
+    /// Coral
+    public class var coral: UIColor { get { return UIColor.rgb(255, 127, 80)}}
+    /// CornFlowerBlue
+    public class var cornFlowerBlue: UIColor { get { return UIColor.rgb(100, 149, 237)}}
+    /// CornSilk
+    public class var cornSilk: UIColor { get { return UIColor.rgb(255, 248, 220)}}
+    /// Crimson
+    public class var crimson: UIColor { get { return UIColor.rgb(220, 20, 60)}}
+    /// DarkBlue
+    public class var darkBlue: UIColor { get { return UIColor.rgb(0, 0, 139)}}
+    /// DarkCyan
+    public class var darkCyan: UIColor { get { return UIColor.rgb(0, 139, 139)}}
+    /// DarkGoldenRod
+    public class var darkGoldenRod: UIColor { get { return UIColor.rgb(184, 134, 11)}}
+    /// DarkGreen
+    public class var darkGreen: UIColor { get { return UIColor.rgb(0, 100, 0)}}
+    /// DarkGren
+    public class var darkGrey: UIColor { get { return UIColor.rgb(169, 169, 169)}}
+    /// DarkKhaki
+    public class var darkKhaki: UIColor { get { return UIColor.rgb(189, 183, 107)}}
+    /// DarkMagenta
+    public class var darkMagenta: UIColor { get { return UIColor.rgb(139, 0, 139)}}
+    /// DarkOliveGreen
+    public class var darkOliveGreen: UIColor { get { return UIColor.rgb(85, 107, 47)}}
+    /// DarkOrange
+    public class var darkOrange: UIColor { get { return UIColor.rgb(255, 140, 0)}}
+    /// DarkOrchid
+    public class var darkOrchid: UIColor { get { return UIColor.rgb(153, 50, 204)}}
+    /// DarkRed
+    public class var darkRed: UIColor { get { return UIColor.rgb(139, 0, 0)}}
+    /// DarkSalmon
+    public class var darkSalmon: UIColor { get { return UIColor.rgb(233, 150, 122)}}
+    /// DarkSlateBlue
+    public class var darkSlateBlue: UIColor { get { return UIColor.rgb(72, 61, 139)}}
+    /// DarkSlateGray
+    public class var darkSlateGray: UIColor { get { return UIColor.rgb(47, 79, 79)}}
+    /// DarkTurquoise
+    public class var darkTurquoise: UIColor { get { return UIColor.rgb(0, 206, 209)}}
+    /// DarkViolet
+    public class var darkViolet: UIColor { get { return UIColor.rgb(148, 0, 211)}}
+    /// DeepPink
+    public class var deepPink: UIColor { get { return UIColor.rgb(255, 20, 147)}}
+    /// DeepSkyBlue
+    public class var deepSkyBlue: UIColor { get { return UIColor.rgb(0, 191, 255)}}
+    /// DimGray
+    public class var dimGray: UIColor { get { return UIColor.rgb(105, 105, 105)}}
+    /// DodgerBlue
+    public class var godgerBlue: UIColor { get { return UIColor.rgb(30, 144, 255)}}
+    /// FireBrick
+    public class var fierBrick: UIColor { get { return UIColor.rgb(178, 34, 34)}}
+    /// FloralWhite
+    public class var floralWhite: UIColor { get { return UIColor.rgb(255, 250, 240)}}
+    /// ForestGreen
+    public class var forestGreen: UIColor { get { return UIColor.rgb(34, 139, 34)}}
+    /// Fuchsia
+    public class var fuchsia: UIColor { get { return UIColor.rgb(255, 0, 255)}}
+    /// Gainsboro
+    public class var gainsboro: UIColor { get { return UIColor.rgb(220, 220, 220)}}
+    /// Gold
+    public class var gold: UIColor { get { return UIColor.rgb(255, 215, 0)}}
+    /// GoldenRod
+    public class var goldenRod: UIColor { get { return UIColor.rgb(218, 165, 32)}}
+    /// GreenYellow
+    public class var greenYellow: UIColor { get { return UIColor.rgb(173, 255, 47)}}
+    /// HalfClear
+    public class var halfClear: UIColor { get { return UIColor.rgba(0, 0, 0, 0.5)}}
+    /// Honeydew
+    public class var honeydew: UIColor { get { return UIColor.rgb(240, 255, 240)}}
+    /// HotPink
+    public class var hotPink: UIColor { get { return UIColor.rgb(255, 105, 180)}}
+    /// IndianRed
+    public class var indianRed: UIColor { get { return UIColor.rgb(205, 92, 92)}}
+    /// Indigo
+    public class var indigo: UIColor { get { return UIColor.rgb(75, 0, 130)}}
+    /// Ivory
+    public class var ivory: UIColor { get { return UIColor.rgb(255, 255, 240)}}
+    /// Khaki
+    public class var khkai: UIColor { get { return UIColor.rgb(240, 230, 140)}}
+    /// Lavender
+    public class var lavender: UIColor { get { return UIColor.rgb(230, 230, 250)}}
+    /// LavenderBlush
+    public class var lavenderBlush: UIColor { get { return UIColor.rgb(255, 240, 245)}}
+    /// LawnGreen
+    public class var lawnGreen: UIColor { get { return UIColor.rgb(124, 252, 0)}}
+    /// LemonChiffon
+    public class var lemonChiffon: UIColor { get { return UIColor.rgb(255, 250, 205)}}
+    /// LightBlue
+    public class var lightBlue: UIColor { get { return UIColor.rgb(173, 216, 230)}}
+    /// LightCoral
+    public class var lightCoral: UIColor { get { return UIColor.rgb(240, 128, 128)}}
+    /// LightCyan
+    public class var lightCyan: UIColor { get { return UIColor.rgb(224, 255, 255)}}
+    /// LightGoldenRodYellow
+    public class var lightGoldenRodYellow: UIColor { get { return UIColor.rgb(250, 250, 210)}}
+    /// LightGreen
+    public class var lightGreen: UIColor { get { return UIColor.rgb(144, 238, 144)}}
+    /// LightPink
+    public class var lightPink: UIColor { get { return UIColor.rgb(255, 182, 193)}}
+    /// LightSalmon
+    public class var lightSalmon: UIColor { get { return UIColor.rgb(255, 160, 122)}}
+    /// LightSeaGreen
+    public class var lightSeaGreen: UIColor { get { return UIColor.rgb(32, 178, 170)}}
+    /// LightSkyBlue
+    public class var lightSkyBlue: UIColor { get { return UIColor.rgb(135, 206, 250)}}
+    /// LightSlateGray
+    public class var lightSlateGray: UIColor { get { return UIColor.rgb(119, 136, 153)}}
+    /// LightSteelBlue
+    public class var lightSteelBlue: UIColor { get { return UIColor.rgb(176, 196, 222)}}
+    /// LightYellow
+    public class var lightYellow: UIColor { get { return UIColor.rgb(255, 255, 224)}}
+    /// Lime
+    public class var lime: UIColor { get { return UIColor.rgb(0, 255, 0)}}
+    /// LimeGreen
+    public class var limeGreen: UIColor { get { return UIColor.rgb(50, 205, 50)}}
+    /// Linen
+    public class var linen: UIColor { get { return UIColor.rgb(250, 240, 230)}}
+    /// Maroon
+    public class var maroon: UIColor { get { return UIColor.rgb(128, 0, 0)}}
+    /// MediumAquamarine
+    public class var mediumAuamarine: UIColor { get { return UIColor.rgb(102, 205, 170)}}
+    /// MediumBlue
+    public class var mediumBlue: UIColor { get { return UIColor.rgb(0, 0, 205)}}
+    /// MediumOrchid
+    public class var mediumOrchid: UIColor { get { return UIColor.rgb(186, 85, 211)}}
+    /// MediumSeaGreen
+    public class var mediumSeaGreen: UIColor { get { return UIColor.rgb(60, 179, 113)}}
+    /// MediumSlateBlue
+    public class var mediumSlateBlue: UIColor { get { return UIColor.rgb(123, 104, 238)}}
+    /// MediumSpringGreen
+    public class var mediumSpringGreen: UIColor { get { return UIColor.rgb(0, 250, 154)}}
+    /// MediumTorquoise
+    public class var mediumTorquoise: UIColor { get { return UIColor.rgb(72, 209, 204)}}
+    /// MediumVioletRed
+    public class var mediumVioletRed: UIColor { get { return UIColor.rgb(199, 21, 133)}}
+    /// MidNightBlue
+    public class var midNightBlue: UIColor { get { return UIColor.rgb(25, 25, 112)}}
+    /// MistyRose
+    public class var mistyRose: UIColor { get { return UIColor.rgb(255, 228, 225)}}
+    /// Moccasin
+    public class var moccasin: UIColor { get { return UIColor.rgb(255, 228, 181)}}
+    /// NavajoWhite
+    public class var navajoWhite: UIColor { get { return UIColor.rgb(255, 222, 173)}}
+    /// Navy
+    public class var navy: UIColor { get { return UIColor.rgb(0, 0, 128)}}
+    /// OldLace
+    public class var oldLace: UIColor { get { return UIColor.rgb(253, 245, 230)}}
+    /// Olive
+    public class var olive: UIColor { get { return UIColor.rgb(128, 128, 0)}}
+    /// OliveDrab
+    public class var oliveDrab: UIColor { get { return UIColor.rgb(107, 142, 35)}}
+    /// OrangeRed
+    public class var orangeRed: UIColor { get { return UIColor.rgb(255, 69, 0)}}
+    /// Orchid
+    public class var orchid: UIColor { get { return UIColor.rgb(218, 112, 214)}}
+    /// PaleGoldenRod
+    public class var paleGoldenRod: UIColor { get { return UIColor.rgb(238, 232, 170)}}
+    /// PaleGreen
+    public class var pageGreen: UIColor { get { return UIColor.rgb(152, 251, 152)}}
+    /// PaleTurquoise
+    public class var paleTurquoise: UIColor { get { return UIColor.rgb(175, 238, 238)}}
+    /// PaleVioletRed
+    public class var paleVioletRed: UIColor { get { return UIColor.rgb(219, 112, 147)}}
+    /// Papayawhip
+    public class var papayawhip: UIColor { get { return UIColor.rgb(255, 239, 213)}}
+    /// PeachPuff
+    public class var peachPuff: UIColor { get { return UIColor.rgb(255, 218, 185)}}
+    /// Peru
+    public class var peru: UIColor { get { return UIColor.rgb(205, 133, 63)}}
+    /// Plum
+    public class var plum: UIColor { get { return UIColor.rgb(221, 160, 221)}}
+    /// PowderBlue
+    public class var powderBlue: UIColor { get { return UIColor.rgb(176, 224, 230)}}
+    /// RosyBrown
+    public class var rosyBrown: UIColor { get { return UIColor.rgb(188, 143, 143)}}
+    /// RoyalBlue
+    public class var royalBlue: UIColor { get { return UIColor.rgb(65, 105, 225)}}
+    /// SaddleBrown
+    public class var saddleBrown: UIColor { get { return UIColor.rgb(139, 69, 19)}}
+    /// Salmon
+    public class var salmon: UIColor { get { return UIColor.rgb(250, 128, 114)}}
+    /// SandyBrown
+    public class var sandyBrown: UIColor { get { return UIColor.rgb(244, 164, 96)}}
+    /// SeaGreen
+    public class var seaGreen: UIColor { get { return UIColor.rgb(46, 139, 87)}}
+    /// SeaShell
+    public class var seaShell: UIColor { get { return UIColor.rgb(255, 245, 238)}}
+    /// Sienna
+    public class var sienna: UIColor { get { return UIColor.rgb(160, 82, 45)}}
+    /// Silver
+    public class var silver: UIColor { get { return UIColor.rgb(192, 192, 192)}}
+    /// SkyBlue
+    public class var skyBlue: UIColor { get { return UIColor.rgb(135, 206, 235)}}
+    /// SlateBlue
+    public class var slateBlue: UIColor { get { return UIColor.rgb(106, 90, 205)}}
+    /// SlateGray
+    public class var slateGray: UIColor { get { return UIColor.rgb(112, 128, 144)}}
+    /// Snow
+    public class var snow: UIColor { get { return UIColor.rgb(255, 250, 250)}}
+    /// SpringGreen
+    public class var springGreen: UIColor { get { return UIColor.rgb(0, 255, 127)}}
+    /// SteelBlue
+    public class var steelBlue: UIColor { get { return UIColor.rgb(70, 130, 180)}}
+    /// Tan
+    public class var tan: UIColor { get { return UIColor.rgb(210, 180, 140)}}
+    /// Teal
+    public class var teal: UIColor { get { return UIColor.rgb(0, 128, 128)}}
+    /// Thistle
+    public class var thistle: UIColor { get { return UIColor.rgb(216, 191, 216)}}
+    /// Tomato
+    public class var tomato: UIColor { get { return UIColor.rgb(255, 99, 71)}}
+    /// Turquoise
+    public class var turquoise: UIColor { get { return UIColor.rgb(64, 224, 208)}}
+    /// Violet
+    public class var violet: UIColor { get { return UIColor.rgb(238, 130, 238)}}
+    /// Wheat
+    public class var wheat: UIColor { get { return UIColor.rgb(245, 222, 179)}}
+    /// YellowGreen
+    public class var yellowGreen: UIColor { get { return UIColor.rgb(154, 205, 50)}}
+
+
+    /// Instanced color with R/G/B. default A = 1
+    ///
+    /// - Parameter rgb: Red/Blue/Green, the value of R/G/B can be between 0 with 255, also 0~1
+    /// - Returns: Instacne of color
+    public class func rgb(_ rgb: CGFloat...) -> UIColor {
+        assert(rgb.count == 3, "Usage: it must contain 3 values \t\n UIColor.rgb(255, 255, 255)")
+        let colors = rgb.map({ $0 > 1 ? ($0 / 255.0) : $0})
+        return UIColor(red: colors[0], green: colors[1], blue: colors[2], alpha: 1)
+    }
+
+    /// Instanced color with R/G/B/A.
+    ///
+    /// - Parameter rgba: Red/Blue/Green/Alpha, the value of R/G/B/A can be between 0 with 255, also 0~1
+    /// - Returns: Instacne of color
+    public class func rgba(_ rgba: CGFloat...) -> UIColor {
+        assert(rgba.count == 4, "Usage: it must contain 4 values \t\n UIColor.rgba(255, 255, 255, 1.0)")
+        let colors = rgba.map({ $0 > 1 ? ($0 / 255.0) : $0})
+        return UIColor(red: colors[0], green: colors[1], blue: colors[2], alpha: colors[3])
+    }
+
+
+    /// Using hex string to init color
+    ///
+    /// - Parameter hex: The hex string.
+    ///                   Formats:  [0x | OX][ff00ff00 | ff00ff | f0f0 | fff]
+    ///                                      [RRGGBBAA | RRGGBB | RGBA | RGB]
+    public convenience init(hex: String) {
+
+        /// Reject strings
+        let hexString = hex.replacingOccurrences(of: "0x", with: "")
+        let valuesStr = hexString.replacingOccurrences(of: "0X", with: "")
+
+        /// Process values
+        var r = 0, g = 0, b = 0, a = 1
+        switch valuesStr.count {
+        case 8, 6:
+            r = Int(valuesStr[0...1], radix: 16) ?? 0
+            g = Int(valuesStr[2...3], radix: 16) ?? 0
+            b = Int(valuesStr[4...5], radix: 16) ?? 0
+            if valuesStr.count == 8 {
+                a = Int(valuesStr[6...7], radix: 16) ?? 0
+            }
+        case 4, 3:
+            r = Int(valuesStr[0...0], radix: 16) ?? 0
+            g = Int(valuesStr[1...1], radix: 16) ?? 0
+            b = Int(valuesStr[2...2], radix: 16) ?? 0
+            if valuesStr.count == 4 {
+                a = Int(valuesStr[3...3], radix: 16) ?? 0
+            }
+        default:
+            break
+        }
+        print("Color's RGBA is: \(r)\t\(g)\t\(b)\t\(a)")
+        self.init(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: CGFloat(a))
+    }
+}
--- SwiftUtils/SUStringUtils.swift
+++ SwiftUtils/SUStringUtils.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+public extension String {
+
+    /// 利用 ..< 运算符获取子字符串
+    ///
+    /// - Parameter range: 范围
+    public subscript(_ range: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end   = index(startIndex, offsetBy: range.upperBound)
+
+        assert(start <= endIndex && end < endIndex, "Beyond string's range...")
+        return self[start..<end]
+    }
+
+
+    /// 利用 ... 运算符，获取子字符串
+    ///
+    /// - Parameter range: 范围
+    public subscript(_ range: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end   = index(startIndex, offsetBy: range.upperBound)
+
+        assert(start <= endIndex && end < endIndex, "Beyond string's range...")
+        return self[start...end]
+    }
+}
